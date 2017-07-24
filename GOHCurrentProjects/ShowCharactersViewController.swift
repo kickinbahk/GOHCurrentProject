9893aa1286823f14c4c94d09d6af5ae6a3603e6c
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-07-24 16:25:33 -0700

    add tableview

--- GOHCurrentProjects/ShowCharactersViewController.swift
+++ GOHCurrentProjects/ShowCharactersViewController.swift
@@ -16,3 +16,12 @@ class ShowCharactersViewController: UIViewController {
 
 }
 
+extension ShowCharactersViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
